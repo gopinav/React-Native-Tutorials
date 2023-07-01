79c7fd99ea99584bdd9684008df7be7a561ccbab
Vishwas <vishwas.g5@gmail.com>
2023-07-01 18:26:04 +0530

    View component

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum" }}>
+      <View style={{ backgroundColor: "lightblue", height: 200, width: 200 }} />
+      <View
+        style={{ backgroundColor: "lightgreen", height: 200, width: 200 }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
