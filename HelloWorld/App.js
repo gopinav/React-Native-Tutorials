bfbf62b7a7db2ae06dfdfc628b7c256219d83beb
Vishwas <vishwas.g5@gmail.com>
2023-07-01 21:42:41 +0530

    Nested Text component

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -2,12 +2,10 @@ import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View style={{ backgroundColor: "lightblue", height: 200, width: 200 }} />
-      <View
-        style={{ backgroundColor: "lightgreen", height: 200, width: 200 }}
-      />
-      <Text>Hello World!</Text>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
+      <Text>
+        <Text style={{ color: "white" }}>Hello</Text> World!
+      </Text>
     </View>
   );
 }
