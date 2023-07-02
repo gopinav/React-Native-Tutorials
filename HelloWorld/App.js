5d5e8dab9cca12762d558a4544d91f51a4b58363
Vishwas <vishwas.g5@gmail.com>
2023-07-02 17:12:16 +0530

    Image component

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 50 }}>
-      <Text>
-        <Text style={{ color: "white" }}>Hello</Text> World!
-      </Text>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      {/* <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/200" }}
+        style={{ width: 200, height: 200 }}
+      /> */}
+      {/* <ImageBackground source={logoImg} style={{ flex: 1 }}>
+        <Text>
+          <Text style={{ color: "white" }}>Hello</Text> World!
+        </Text>
+      </ImageBackground> */}
     </View>
   );
 }
