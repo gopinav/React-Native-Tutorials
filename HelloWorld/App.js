2abeca8b76907437455ba107b3941821ee85b391
Vishwas <vishwas.g5@gmail.com>
2023-07-03 15:45:47 +0530

    Modal component

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -6,17 +6,22 @@ import {
   ScrollView,
   Button,
   Pressable,
+  Modal,
 } from "react-native";
+import { useState } from "react";
 
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
       <ScrollView>
         <Button
           title="Click me"
-          onPress={() => console.log("Button pressed")}
+          // onPress={() => StatusBar.setHidden(true)}
+          onPress={() => setIsModalVisible(true)}
+          // onPress={() => console.log("Button pressed")}
           color="midnightblue"
         />
         <Pressable
@@ -64,6 +69,21 @@ export default function App() {
         </Text>
       </ImageBackground> */}
       </ScrollView>
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={() => setIsModalVisible(false)}
+        animationType="slide"
+        presentationStyle="formSheet"
+      >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+          <Text>Modal content</Text>
+          <Button
+            title="Close"
+            onPress={() => setIsModalVisible(false)}
+            color="midnightblue"
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
