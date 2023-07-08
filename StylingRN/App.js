89e246c103e6db70fdbce904e49640242fa3ce37
Vishwas <vishwas.g5@gmail.com>
2023-07-08 15:48:40 +0530

    Box model styles

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -4,7 +4,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={[styles.box, styles.lightblueBg]}>
-        <Text>Lightblue box</Text>
+        <Text style={{ borderRadius: 5, backgroundColor: "red" }}>
+          Lightblue box
+        </Text>
       </View>
       <View style={[styles.box, styles.lightgreenBg]}>
         <Text>Lightgreen box</Text>
@@ -20,9 +22,14 @@ const styles = StyleSheet.create({
     padding: 60,
   },
   box: {
-    width: 100,
-    height: 100,
-    padding: 10,
+    width: "25%",
+    height: "25%",
+    // padding: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "purple",
   },
   lightblueBg: {
     backgroundColor: "lightblue",
