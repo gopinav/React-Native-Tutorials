e3d00033b54f404a8c606a10f05f6efaed6e1e58
Vishwas <vishwas.g5@gmail.com>
2023-07-28 18:49:46 +0530

    Layout code setup

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import Box from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
+      <Box style={{ backgroundColor: "#01243a" }}>Box 8</Box>
+      <Box style={{ backgroundColor: "#7aa100" }}>Box 9</Box>
     </View>
   );
 }
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red",
   },
 });
---
+++ RNLayout/components/Box.js
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Box({ children, style }) {
+  return (
+    <View style={[styles.box, style]}>
+      <Text style={styles.text}>{children}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
+});
