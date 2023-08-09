ab4fa6a1527f5bb583dab16ac22184069a23208d
Vishwas <vishwas.g5@gmail.com>
2023-08-09 18:36:33 +0530

    Dimensions API

--- DynamicUI/App.js
+++ DynamicUI/App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, Image, Dimensions } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome!</Text>
+      </View>
     </View>
   );
 }
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    width: windowWidth > 500 ? "70%" : "90%",
+    height: windowHeight > 600 ? "60%" : "90%",
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: windowWidth > 500 ? 50 : 24,
   },
 });
