6d5e9263a9f9a00fdac76ed0bc81d80041fd6399
Vishwas <vishwas.g5@gmail.com>
2023-08-11 18:41:09 +0530

    Listening to dimension changes

--- DynamicUI/App.js
+++ DynamicUI/App.js
@@ -1,17 +1,44 @@
-import { StyleSheet, View, Text, Image, Dimensions } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
 
 export default function App() {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  });
+
+  const { window } = dimensions;
+  const windowWidth = window.width;
+  const windowHeight = window.height;
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {
+      setDimensions({ window });
+    });
+    return () => subscription?.remove();
+  });
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome!</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 600 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text
+          style={{
+            fontSize: windowWidth > 500 ? 50 : 24,
+          }}
+        >
+          Welcome!
+        </Text>
       </View>
     </View>
   );
 }
-
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
@@ -21,13 +48,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   box: {
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 600 ? "60%" : "90%",
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 600 ? "60%" : "90%",
     backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    fontSize: windowWidth > 500 ? 50 : 24,
-  },
+  // text: {
+  //   fontSize: windowWidth > 500 ? 50 : 24,
+  // },
 });
