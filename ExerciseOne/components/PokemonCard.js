02028eb427c7120655f39732c2a3f0f04babcae4
Vishwas <vishwas.g5@gmail.com>
2023-08-15 14:07:00 +0530

    Part 5

--- ExerciseOne/components/PokemonCard.js
+++ ExerciseOne/components/PokemonCard.js
@@ -1,5 +1,20 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 
+const getTypeDetails = (type) => {
+  switch (type.toLowerCase()) {
+    case "electric":
+      return { borderColor: "#FFD700", emoji: "⚡️" };
+    case "water":
+      return { borderColor: "#6493EA", emoji: "💧" };
+    case "fire":
+      return { borderColor: "#FF5733", emoji: "🔥" };
+    case "grass":
+      return { borderColor: "#66CC66", emoji: "🌿" };
+    default:
+      return { borderColor: "#A0A0A0", emoji: "❓" };
+  }
+};
+
 export default function PokemonCard({
   name,
   image,
@@ -8,25 +23,36 @@ export default function PokemonCard({
   moves,
   weaknesses,
 }) {
+  const { borderColor, emoji } = getTypeDetails(type);
   return (
     <View style={styles.card}>
-      <View>
-        <Text>{name}</Text>
-        <Text>HP: {hp}</Text>
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.hp}>❤️HP: {hp}</Text>
       </View>
 
-      <Image source={image} accessibilityLabel={`${name} Pokemon`} />
+      <Image
+        source={image}
+        accessibilityLabel={`${name} Pokemon`}
+        style={styles.image}
+        resizeMode="contain"
+      />
 
-      <View>
-        <Text>{type}</Text>
+      <View style={styles.typeContainer}>
+        <View style={[styles.badge, { borderColor }]}>
+          <Text style={styles.typeEmoji}>{emoji}</Text>
+          <Text style={styles.typeText}>{type}</Text>
+        </View>
       </View>
 
-      <View>
-        <Text>Moves: {moves.join(", ")}</Text>
+      <View style={styles.movesContainer}>
+        <Text style={styles.movesText}>Moves: {moves.join(", ")}</Text>
       </View>
 
-      <View>
-        <Text>Weakness: {weaknesses.join(", ")}</Text>
+      <View style={styles.weaknessContainer}>
+        <Text style={styles.weaknessText}>
+          Weakness: {weaknesses.join(", ")}
+        </Text>
       </View>
     </View>
   );
@@ -51,5 +77,59 @@ const styles = StyleSheet.create({
         elevation: 5,
       },
     }),
+  },
+  nameContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    marginBottom: 32,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  hp: {
+    fontSize: 22,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 16,
+  },
+  typeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 40,
+  },
+  badge: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    borderWidth: 4,
+  },
+  typeEmoji: {
+    fontSize: 30,
+    marginRight: 12,
+  },
+  typeText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  movesContainer: {
+    marginBottom: 12,
+  },
+  movesText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  weaknessContainer: {
+    marginBottom: 8,
+  },
+  weaknessText: {
+    fontSize: 22,
+    fontWeight: "bold",
   },
 });
