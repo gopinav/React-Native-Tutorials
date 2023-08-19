9d5ce10eb7bccef013343e8213b2c758e82112c0
Vishwas <vishwas.g5@gmail.com>
2023-08-19 14:50:57 +0530

    Rendering lists

--- RNList/App.js
+++ RNList/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import PokemonList from "./data.json";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeContainer}>
+      <ScrollView style={styles.container}>
+        {PokemonList.map((pokemon) => (
+          <View style={styles.card} key={pokemon.id}>
+            <Text>
+              {pokemon.type}: {pokemon.name}
+            </Text>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    paddingTop: 32,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5F5F5",
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+    borderWidth: 1,
   },
 });
