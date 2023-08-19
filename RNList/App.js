d98529e68c8e3fd8030cc1b5df0361752e4158b5
Vishwas <vishwas.g5@gmail.com>
2023-08-19 18:41:08 +0530

    FlatList

--- RNList/App.js
+++ RNList/App.js
@@ -1,31 +1,54 @@
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
-import PokemonList from "./data.json";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  FlatList,
+  StatusBar,
+} from "react-native";
+import pokemonList from "./data.json";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeContainer}>
-      <ScrollView style={styles.container}>
-        {PokemonList.map((pokemon) => (
-          <View style={styles.card} key={pokemon.id}>
-            <Text>
-              {pokemon.type}: {pokemon.name}
-            </Text>
-          </View>
-        ))}
-      </ScrollView>
+    <SafeAreaView styles={styles.container}>
+      {/* <ScrollView style={styles.scrollView}>
+        {pokemonList.map((pokemon) => {
+          console.log(pokemon.id);
+          return (
+            <View style={styles.card} key={pokemon.id}>
+              <Text style={styles.cardText}>{pokemon.type}</Text>
+              <Text style={styles.cardText}>{pokemon.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView> */}
+      <View style={styles.scrollView}>
+        <FlatList
+          data={pokemonList}
+          renderItem={({ item }) => {
+            console.log(item.id);
+            return (
+              <View style={styles.card}>
+                <Text style={styles.cardText}>{item.type}</Text>
+                <Text style={styles.cardText}>{item.name}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  safeContainer: {
-    flex: 1,
-    backgroundColor: "#F5F5F5",
-    paddingTop: 32,
-  },
   container: {
     flex: 1,
     backgroundColor: "#F5F5F5",
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
     paddingHorizontal: 16,
   },
   card: {
@@ -34,5 +57,8 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginBottom: 16,
     borderWidth: 1,
+  },
+  cardText: {
+    fontSize: 30,
   },
 });
