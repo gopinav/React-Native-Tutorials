729a3b5f8ca4e1fbfff3f6af638542f9c2cbd30e
Vishwas <vishwas.g5@gmail.com>
2023-08-26 11:58:13 +0530

    Multiline Input and Switch

--- RNForms/App.js
+++ RNForms/App.js
@@ -4,15 +4,39 @@ import {
   TextInput,
   SafeAreaView,
   StatusBar,
+  Switch,
+  View,
 } from "react-native";
 import { useState } from "react";
 
 export default function App() {
   const [name, setName] = useState("");
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="vishwas@example.com"
+        secureTextEntry={true}
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={[styles.input, styles.multilineText]}
+        placeholder="message"
+        multiline
+      />
       <Text style={styles.text}>My name is {name}</Text>
+      <View style={styles.switchContainer}>
+        <Text style={styles.text}>Dark Mode</Text>
+        <Switch
+          value={isDarkMode}
+          onValueChange={() => setIsDarkMode((previousState) => !previousState)}
+          trackColor={{ false: "#767577", true: "lightblue" }}
+          thumbColor="#f4f3f4"
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -22,8 +46,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     paddingTop: StatusBar.currentHeight,
-    // alignItems: "center",
-    // justifyContent: "center",
   },
   input: {
     height: 40,
@@ -34,5 +56,15 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
     padding: 10,
+  },
+  multilineText: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
   },
 });
