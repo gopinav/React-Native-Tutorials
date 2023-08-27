a810c527a5f1b645f279440cc991325baeee81de
Vishwas <vishwas.g5@gmail.com>
2023-08-27 19:17:23 +0530

    Login form

--- RNForms/App.js
+++ RNForms/App.js
@@ -1,70 +1,122 @@
+import React, { useState } from "react";
 import {
-  StyleSheet,
-  Text,
-  TextInput,
-  SafeAreaView,
-  StatusBar,
-  Switch,
   View,
+  TextInput,
+  Button,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Text,
+  Image,
+  Platform,
 } from "react-native";
-import { useState } from "react";
 
-export default function App() {
-  const [name, setName] = useState("");
-  const [isDarkMode, setIsDarkMode] = useState(false);
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    let errors = {};
+
+    if (!username) errors.username = "Username is required";
+    if (!password) errors.password = "Password is required";
+
+    setErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = () => {
+    if (validateForm()) {
+      console.log("Submitted", username, password);
+      setUsername("");
+      setPassword("");
+      setErrors({});
+    }
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <TextInput
-        style={styles.input}
-        value={name}
-        onChangeText={setName}
-        placeholder="vishwas@example.com"
-        secureTextEntry={true}
-        keyboardType="numeric"
-      />
-      <TextInput
-        style={[styles.input, styles.multilineText]}
-        placeholder="message"
-        multiline
-      />
-      <Text style={styles.text}>My name is {name}</Text>
-      <View style={styles.switchContainer}>
-        <Text style={styles.text}>Dark Mode</Text>
-        <Switch
-          value={isDarkMode}
-          onValueChange={() => setIsDarkMode((previousState) => !previousState)}
-          trackColor={{ false: "#767577", true: "lightblue" }}
-          thumbColor="#f4f3f4"
+    <KeyboardAvoidingView
+      behavior="padding"
+      keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
+      style={styles.container}
+    >
+      <View style={styles.form}>
+        <Image
+          source={require("./assets/adaptive-icon.png")}
+          style={{
+            width: 200,
+            height: 400,
+            alignSelf: "center",
+            marginBottom: 50,
+          }}
         />
+        <Text style={styles.label}>Username</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your username"
+          value={username}
+          onChangeText={setUsername}
+        />
+        {errors.username ? (
+          <Text style={styles.errorText}>{errors.username}</Text>
+        ) : null}
+
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        {errors.password ? (
+          <Text style={styles.errorText}>{errors.password}</Text>
+        ) : null}
+
+        <Button title="Login" onPress={handleSubmit} />
       </View>
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  form: {
+    backgroundColor: "#ffffff",
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    fontWeight: "bold",
   },
   input: {
     height: 40,
-    margin: 12,
+    borderColor: "#ddd",
     borderWidth: 1,
+    marginBottom: 15,
     padding: 10,
+    borderRadius: 5,
   },
-  text: {
-    fontSize: 30,
-    padding: 10,
-  },
-  multilineText: {
-    minHeight: 100,
-    textAlignVertical: "top",
-  },
-  switchContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 10,
+  errorText: {
+    color: "red",
+    marginBottom: 10,
   },
 });
+
+export default LoginForm;
