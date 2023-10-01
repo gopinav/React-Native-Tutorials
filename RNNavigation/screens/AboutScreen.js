a529a7b56ca05aae584ed7419e39b76236227fb5
Vishwas <vishwas.g5@gmail.com>
2023-10-01 19:04:06 +0530

    Passing data between screens

--- RNNavigation/screens/AboutScreen.js
+++ RNNavigation/screens/AboutScreen.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-export default function AboutScreen({ route }) {
+export default function AboutScreen({ navigation, route }) {
   const { name } = route.params;
   return (
     <View style={styles.container}>
       <Text style={styles.text}>About {name}</Text>
+      <Button
+        title="Update the name"
+        onPress={() => navigation.setParams({ name: "Codevolution" })}
+      />
+      <Button
+        title="Go back with data"
+        onPress={() => {
+          navigation.navigate("Home", { result: "Data from About" });
+        }}
+      />
     </View>
   );
 }
--- RNNavigation/screens/HomeScreen.js
+++ RNNavigation/screens/HomeScreen.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 
-export default function HomeScreen({ navigation }) {
+export default function HomeScreen({ navigation, route }) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
@@ -10,6 +9,7 @@ export default function HomeScreen({ navigation }) {
         title="Go to About"
         onPress={() => navigation.navigate("About", { name: "Vishwas" })}
       />
+      <Text style={styles.text}>Result: {route.params?.result}</Text>
     </View>
   );
 }
