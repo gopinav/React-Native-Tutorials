ac2dc3bd72637611fa5bdb8f1c9e8bb32d8c00ae
Vishwas <vishwas.g5@gmail.com>
2023-10-01 16:29:59 +0530

    Screen navigation

--- RNNavigation/screens/HomeScreen.js
+++ RNNavigation/screens/HomeScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Button
+        title="Go to About"
+        onPress={() => navigation.navigate("About")}
+      />
     </View>
   );
 }
